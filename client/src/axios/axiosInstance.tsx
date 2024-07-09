70763016a36b004e29500f515715e616805880fe
Assanali18 <ustaevasanali@gmail.com>
2024-07-10 03:50:53 +0500

    deploy back

--- client/src/axios/axiosInstance.tsx
+++ client/src/axios/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://seruen-production.up.railway.app',
     headers: {
         'Content-Type': 'application/json'
     }
