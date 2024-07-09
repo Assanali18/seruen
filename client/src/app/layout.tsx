27ff98cc8fd9ea19eb396902b2abe7c3b9c5c93a
Assanali18 <ustaevasanali@gmail.com>
2024-07-10 01:50:29 +0500

    add analytics

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
+      <Analytics/>
       <body className={poppins.className}>{children}</body>
     </html>
   );
