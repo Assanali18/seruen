811ee3134dcf5efe21f02d54807e8c3faf4ad857
Assanali18 <ustaevasanali@gmail.com>
2024-07-14 23:32:16 +0500

    fix chrome again

--- parser-sxodim/src/index.ts
+++ parser-sxodim/src/index.ts
@@ -66,7 +66,8 @@ async function sendEventsToMainServer(events: CreateEventDto[]) {
 
 async function parseEvents() {
   const browser = await puppeteer.launch({
-    ignoreDefaultArgs: ['--disable-extensions'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: process.env.CHROME_BIN || undefined
   });
 
   const page = await browser.newPage();
