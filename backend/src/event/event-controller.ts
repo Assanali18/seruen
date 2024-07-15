0628b6885023480c698f412e4dae989d69d8d58b
Assanali18 <ustaevasanali@gmail.com>
2024-07-15 11:42:38 +0500

    fix db parser

--- backend/src/event/event-controller.ts
+++ backend/src/event/event-controller.ts
@@ -1,33 +1,43 @@
 import { CreateEventDto } from "./dtos/CreateEvent.dto";
 import { Request, Response } from 'express';
-import EventModel  from './models/Event';
+import EventModel from './models/Event';
 
 class EventController {
 
-  
-    createEvents = async(req: Request, res: Response) => {
-      try {
-        const events: CreateEventDto[] = req.body;
-            await EventModel.insertMany(events);
-            console.log('Events received and saved', events.length);
-            
-            res.status(200).send('Events received and saved');
-          } catch (error) {
+    createEvents = async (req: Request, res: Response) => {
+        try {
+            const events: CreateEventDto[] = req.body;
+
+            const titles = events.map(event => event.title);
+
+
+            const existingEvents = await EventModel.find({ title: { $in: titles } });
+            const existingTitles = existingEvents.map(event => event.title);
+
+            const uniqueEvents = events.filter(event => !existingTitles.includes(event.title));
+
+            if (uniqueEvents.length > 0) {
+                await EventModel.insertMany(uniqueEvents);
+                console.log('Events received and saved', uniqueEvents.length);
+                res.status(200).send('Unique events received and saved');
+            } else {
+                res.status(200).send('No unique events to save');
+            }
+        } catch (error) {
             console.error('Error saving events:', error);
             res.status(500).send('Error saving events');
-          }  
+        }
     }
 
-    getEvents = async(req: Request, res: Response) => {
+    getEvents = async (req: Request, res: Response) => {
         try {
             const events = await EventModel.find();
             res.status(200).send(events);
         } catch (error) {
             console.error('Error getting events:', error);
-            res.status(500).send('Error getting events');  
+            res.status(500).send('Error getting events');
         }
     }
 }
-  
-  export default EventController;
-  
+
+export default EventController;
